02cff9d7522c5ad0d293954dd78e266b32fbd1b9
Yasir Ali Mohamed <40922433+Dayax143@users.noreply.github.com>
2022-10-15 20:52:33 +0300

    first commit for django project

---
+++ jumbit/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class JumbitConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'jumbit'
---
+++ jumbit/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+#def home(request):
+    #return HttpResponse("<h1 style='text-align:center; color:blue'>welcome python webApp</h1>")
+
+def dashboard(request):
+    return render(request, 'dashboard.html')
